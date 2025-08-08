eb0f892f7400579c7842dc8aaf0b8724797240fa
sabrata00 <sabrata00@gmail.com>
2025-08-08 21:03:03 +0700

    Deskripsi perubahan yang jelas

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -13,173 +13,110 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text(
-          'Login',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-          ),
-        ),
-        centerTitle: false,
-      ),
+      backgroundColor: Colors.white,
+      appBar: null,
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(24.0),
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           children: [
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             // Logo Section
             Container(
+              width: double.infinity,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(
-                    'TRANSRETAIL',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey[600],
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1.5,
-                    ),
+                  Image.asset(
+                    'assets/TransRetail.png',
+                    height: 20,
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'TRANSmart',
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.0,
-                    ),
+                  SizedBox(height: 8),
+                  Image.asset(
+                    'assets/Transmart.png',
+                    height: 35,
                   ),
                 ],
               ),
             ),
             SizedBox(height: 60),
-            
+
             // Login Form
-            Text(
-              'Masuk',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                color: Colors.black87,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Masuk',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
               ),
             ),
             SizedBox(height: 30),
-            
+
             // ID Field
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  ),
-                ],
+            Card(
+              elevation: 4,
+              shadowColor: Colors.black12,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
               ),
-              child: TextField(
-                controller: _idController,
-                decoration: InputDecoration(
-                  hintText: 'ID',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                  hintStyle: TextStyle(color: Colors.grey[400]),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
                 ),
-              ),
-            ),
-            SizedBox(height: 16),
-            
-            // Password Field
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                  hintStyle: TextStyle(color: Colors.grey[400]),
-                ),
-              ),
-            ),
-            SizedBox(height: 24),
-            
-            // Atau Section
-            Row(
-              children: [
-                Expanded(child: Divider(color: Colors.grey[400])),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                    'Atau',
-                    style: TextStyle(color: Colors.grey[600]),
-                  ),
-                ),
-                Expanded(child: Divider(color: Colors.grey[400])),
-              ],
-            ),
-            SizedBox(height: 24),
-            
-            // Google Login Button
-            Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 24,
-                    width: 24,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
-                    ),
-                    child: Icon(Icons.g_mobiledata, color: Colors.white, size: 18),
-                  ),
-                  SizedBox(width: 12),
-                  Text(
-                    'Masuk dengan Google?',
-                    style: TextStyle(
+                child: TextField(
+                  controller: _idController,
+                  decoration: InputDecoration(
+                    hintText: 'ID',
+                    border: InputBorder.none,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
                       fontSize: 16,
-                      color: Colors.grey[700],
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
-                ],
+                ),
               ),
             ),
             SizedBox(height: 16),
-            
+
+            // Password Field
+            Card(
+              elevation: 4,
+              shadowColor: Colors.black12,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: InputBorder.none,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 24),
+
+            SizedBox(height: 40),
+
             // Register Link
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -200,11 +137,11 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             SizedBox(height: 30),
-            
+
             // Login Button
             Container(
               width: double.infinity,
-              height: 50,
+              height: 55,
               child: ElevatedButton(
                 onPressed: () {
                   // Simulate login success
@@ -214,17 +151,20 @@ class _LoginPageState extends State<LoginPage> {
                   );
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
+                  backgroundColor: Color(0xFFE31837),
+                  elevation: 0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                    borderRadius: BorderRadius.circular(15),
                   ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
                 ),
                 child: Text(
                   'Masuk',
                   style: TextStyle(
-                    fontSize: 18,
+                    fontSize: 17,
                     fontWeight: FontWeight.w600,
                     color: Colors.white,
+                    letterSpacing: 0.5,
                   ),
                 ),
               ),
--- frontend/lib/pages/splash_screen.dart
+++ frontend/lib/pages/splash_screen.dart
@@ -6,11 +6,30 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.forward();
     _navigateToLogin();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   _navigateToLogin() async {
@@ -26,41 +45,32 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.red,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Logo Transretail
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Text(
-                    'TRANSRETAIL',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'TRANSmart',
-                    style: TextStyle(
-                      fontSize: 36,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.5,
-                    ),
-                  ),
-                ],
-              ),
+        child: FadeTransition(
+          opacity: _animation,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/splashA.png',
+                  height: 120,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(height: 20),
+                Image.asset(
+                  'assets/splash.png',
+                  height: 160,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(height: 40),
+                CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeWidth: 3,
+                ),
+              ],
             ),
-            SizedBox(height: 50),
-            CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ],
+          ),
         ),
       ),
     );
