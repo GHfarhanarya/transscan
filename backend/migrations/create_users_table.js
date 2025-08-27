e55b6634917ac87da29b5fa2d26f5161c3d5d1fe
sabrata00 <sabrata00@gmail.com>
2025-08-27 15:01:12 +0700

    sdas

--- backend/migrations/create_users_table.js
+++ backend/migrations/create_users_table.js
@@ -8,24 +8,29 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       employee_id: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(10),
         allowNull: false
       },
       name: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(100),
         allowNull: false
       },
       job_title: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(100),
         allowNull: false
       },
       role: {
-        type: Sequelize.STRING,
+        type: Sequelize.ENUM('admin', 'management', 'staff'),
         allowNull: false
       },
       password: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(255),
         allowNull: false
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
       },
       created_at: {
         allowNull: false,
