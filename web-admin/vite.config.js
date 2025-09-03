6730a6b93e07bda4729d90d6376f1559f26cc741
Syauqi <syauqisyafiq21@gmail.com>
2025-09-03 10:37:04 +0700

    kembali ke konfigurasi sebelumnya

--- web-admin/vite.config.js
+++ web-admin/vite.config.js
@@ -5,27 +5,4 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          utils: ['axios', 'dayjs']
-        }
-      }
-    }
-  },
-  base: '/',
-  server: {
-    port: 3001,
-    host: true
-  },
-  preview: {
-    port: 3001,
-    host: true
-  }
 })
